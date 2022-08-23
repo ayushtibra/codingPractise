5ae14c9e823342060969d5da4053736c1ec78435
ayushtibra <aayushtibra1997@gmail.com>
2022-08-23 17:16:39 +0530

    added this call appy concept learning

---
+++ Concept/this_call_appy.js
@@ -0,0 +1,28 @@
+// https://javascript.plainenglish.io/what-is-the-this-keyword-in-javascript-200c837f5495
+const person = {
+  firstname: "ayush",
+  lastname: "tibra",
+  fullname: function (param1, param2) {
+    console.log(
+      this.firstname + " " + this.lastname + " " + param1 + " " + param2
+    );
+  },
+  tryNew: () => {
+    console.log(this.firstname + " " + this.lastname);
+  },
+};
+
+var Person2 = {
+  firstname: "John",
+  lastname: "Doe",
+};
+
+// this refers to an object. Specifically, the object that is calling upon a function
+person.fullname();
+person.tryNew();
+
+//If this is referenced outside of an object, then it will refer to the global object. This is the default binding.
+console.log(this);
+
+person.fullname.apply(Person2, ["cool", "boy"]);
+person.fullname.call(Person2, "wise", "funny");
