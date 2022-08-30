f2eee0045cfeb1f51375ffddfac3bc7636d73803
ayushtibra <aayushtibra1997@gmail.com>
2022-08-30 18:41:05 +0530

    leetcode 4th array problem median two array

---
+++ CompetitiveCoding/MedianTwoArray(4).js
@@ -0,0 +1,16 @@
+function MedianTwoArray(arr1, arr2) {
+  const mergedArray = [...nums1, ...nums2].sort((a, b) => a - b);
+
+  if (mergedArray.length % 2 != 0) {
+    return mergedArray[Math.floor(mergedArray.length / 2)];
+  }
+
+  return (
+    (mergedArray[mergedArray.length / 2] +
+      mergedArray[mergedArray.length / 2 - 1]) /
+    2
+  );
+}
+
+const result = MedianTwoArray([1, 2], [3, 4]);
+console.log(result);
