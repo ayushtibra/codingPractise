2dc327928f6aab3f45f9d14a9585b0e426b8bbf6
ayushtibra <aayushtibra1997@gmail.com>
2022-09-06 11:44:43 +0530

    leet code easy array problem another nested deep array

---
+++ CompetitiveCoding/RichestCustomerWealth(1672).js
@@ -0,0 +1,22 @@
+var maximumWealth = function (accounts) {
+  const result = accounts.reduce((prev, curr) => {
+    let sumEl = curr.reduce((prev, curr) => {
+      return prev + curr;
+    }, 0);
+    if (sumEl > prev) {
+      prev = sumEl;
+    } else {
+      prev = prev;
+    }
+
+    return prev;
+  }, 0);
+
+  return result;
+};
+
+maximumWealth([
+  [2, 8, 7],
+  [7, 1, 3],
+  [1, 9, 5],
+]);
