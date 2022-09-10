36fa4e613e76485d2be73fed73d4240b4b02c5d7
ayushtibra <aayushtibra1997@gmail.com>
2022-09-10 12:12:27 +0530

    morese code unique array leetcode easy problem

---
+++ CompetitiveCoding/UniqueMorseCodeWords(804).js
@@ -0,0 +1,54 @@
+var uniqueMorseRepresentations = function (words) {
+  const morseCodeArray = [
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
+  ];
+
+  const mapObj = {};
+  const alphaArray = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  for (let i = 0; i < alphaArray.length; i++) {
+    if (!mapObj[alphaArray[i]]) {
+      mapObj[alphaArray[i]] = morseCodeArray[i];
+    }
+  }
+
+  const result = [];
+  words.forEach((element) => {
+    const wordArray = element.split("");
+    let string = "";
+    wordArray.forEach((element) => {
+      string = string + mapObj[element];
+    });
+    result.push(string);
+  });
+
+  return [...new Set(result)].length;
+};
+
+const result = uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]);
+console.log(result);
