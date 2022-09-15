2c2fb4e9ad89c199abfb83cbaa72bf1a12466bf6
ayushtibra <aayushtibra1997@gmail.com>
2022-09-15 18:00:37 +0530

    rocketlane HR tech basic round

---
+++ Interviews/Rocketlane.js
@@ -0,0 +1,9 @@
+// How to set classname in DOM
+// What is functional Programming and how it works
+// How component get re-renders
+// How to set state in hook
+// How to reduce unnecessary re-render in react
+// Different stages of promise
+// How caching works in Browser, which page it needs to cache and which not
+// What is cookies, and how can we use cookies or what are the benefits
+// If you have 10*10 matrix and when you hover over the cell it gets highlighted, how it works
